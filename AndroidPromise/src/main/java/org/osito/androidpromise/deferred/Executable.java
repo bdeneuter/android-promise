a8c742f1597bab363671f8c7562882ec40ea35f5
Bart De Neuter <bart.deneuter@cegeka.be>
2014-02-13 23:23:43 +0100

    when executing task in the background with Function.execute() then run then and onError also on background instead of calling thread

--- AndroidPromise/src/main/java/org/osito/androidpromise/deferred/Executable.java
+++ AndroidPromise/src/main/java/org/osito/androidpromise/deferred/Executable.java
@@ -32,6 +32,60 @@ public class Executable {
         } else {
             executor.execute(runnable);
         }
-        return deferred;
+        return Function.sync ? deferred : new PromiseDecorator<V>(deferred);
+    }
+
+    private static class PromiseDecorator<T> implements Promise<T> {
+
+        private Promise<T> delegate;
+
+        PromiseDecorator(Promise<T> delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public Promise<T> then(Task<T> task) {
+            return delegate.thenOnBackgroundThread(task);
+        }
+
+        @Override
+        public Promise<T> thenOnMainThread(Task<T> task) {
+            return delegate.thenOnMainThread(task);
+        }
+
+        @Override
+        public Promise<T> thenOnBackgroundThread(Task<T> task) {
+            return thenOnBackgroundThread(task);
+        }
+
+        @Override
+        public Promise<T> thenAsync(Task<T> task) {
+            return delegate.thenAsync(task);
+        }
+
+        @Override
+        public Promise<T> onError(Task<Throwable> task) {
+            return delegate.onErrorOnBackgroundThread(task);
+        }
+
+        @Override
+        public Promise<T> onErrorOnMainThread(Task<Throwable> task) {
+            return delegate.onErrorOnMainThread(task);
+        }
+
+        @Override
+        public Promise<T> onErrorOnBackgroundThread(Task<Throwable> task) {
+            return delegate.onErrorOnBackgroundThread(task);
+        }
+
+        @Override
+        public Promise<T> onErrorAsync(Task<Throwable> task) {
+            return delegate.onErrorAsync(task);
+        }
+
+        @Override
+        public <V> Promise<V> convert(Converter<T, V> converter) {
+            return delegate.convert(converter);
+        }
     }
 }
